ce375bab8142ea6e41cbe4fe9142397d7b79347f
Pedro Tavares <pedrodc1236@gmail.com>
2021-11-27 17:41:35 -0300

    exercicio 7 completo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios_pratica/exercicio07.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios_pratica/exercicio07.js
@@ -7,4 +7,20 @@
     Porcentagem < 50 -> F
     O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. */
 
-    let note = 0;
+    let note = 49;
+
+    if (note >= 90 && note <= 100) {
+        console.log("A")
+    } else if (note >= 80 && note < 90){
+        console.log("B")
+    } else if (note >= 70 && note < 80) {
+        console.log("C")
+    } else if (note >= 60 && note < 70) {
+        console.log("D")
+    } else if (note >= 50 && note < 60) {
+        console.log("E")
+    } else if (note >= 0 && note < 50) {
+        console.log ("F")
+    } else if (note < 0 || note > 100) {
+        console.log ("Error")
+    }
