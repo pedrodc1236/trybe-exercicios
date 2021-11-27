c573eb0b00795eaf32a20d7b2eee1622dde3b044
Pedro Tavares <pedrodc1236@gmail.com>
2021-11-27 17:13:34 -0300

    exercicios de 1 a 7 resolvidos menos o 5

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios_pratica/exercicio02.js
@@ -0,0 +1,14 @@
+/* 2. Faça um programa que retorne o maior de dois números. 
+  Defina no começo do programa duas constantes com os valores que serão comparados. */
+
+    const number1 = 24;
+    const number2 = 24;
+
+    if (number1 > number2) {
+        console.log(number1)
+    } else if (number2 > number1) {
+        console.log(number2)
+    } else {
+        console.log("numeros iguais")
+    }
+
