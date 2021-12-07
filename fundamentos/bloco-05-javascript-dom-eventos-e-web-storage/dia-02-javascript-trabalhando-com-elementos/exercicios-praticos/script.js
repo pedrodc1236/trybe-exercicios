068430242683e9a4a4d4708745e59690b9b1fa2e
Pedro Tavares <pedrodc1236@gmail.com>
2021-12-07 01:28:33 -0300

    todos exercicios/5.2 concluidos

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios-praticos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios-praticos/script.js
@@ -1,3 +1,4 @@
+    // PARTE 01
 /* 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ; */
 
     let captureBody = document.getElementsByTagName("body")[0];
@@ -94,4 +95,43 @@
         captureMain.appendChild(createChildsMain);
     }
 
- 
+    // PARTE 02
+    // Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
+
+
+/* 1. Adicione a classe title na tag h1 criada; */
+    
+    let captureH1 = captureBody.firstElementChild.nextElementSibling;
+    captureH1.className = "title";
+
+/* 2. Adicione a classe description nas 3 tags h3 criadas; */
+
+  let captureTags1 = captureMain.lastElementChild;
+  captureTags1.className = "description"
+  let captureTags2 = captureMain.lastElementChild.previousElementSibling;
+  captureTags2.className = "description"
+  let captureTags3 = captureMain.lastElementChild.previousElementSibling.previousElementSibling;
+  captureTags3.className = "description"
+
+/* 3. Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ; */
+
+    captureMain.removeChild(captureSectionPasso5);
+
+/* 4. Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ; */
+
+    captureSectionPasso6.style.marginRight = "auto";
+
+ /* 5. Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde; */
+
+   let captureFather = sectionChildMain.parentElement;
+    captureFather.style.background = "rgb(76,164,109)";
+
+/* 6. Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8. */
+
+    let captureToRemove9Li = sectionChildMain3.firstElementChild.lastElementChild;
+
+    let captureToRemove10Li = sectionChildMain3.firstElementChild.lastElementChild.previousElementSibling;
+
+    listOl.removeChild(captureToRemove9Li);
+    listOl.removeChild(captureToRemove10Li);
+
