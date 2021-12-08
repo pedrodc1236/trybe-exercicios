4b88de026abac42e74744f64ec0455d5651e2487
Pedro Tavares <pedrodc1236@gmail.com>
2021-12-08 14:36:50 -0300

    todos exercicios praticos resolvidos

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios-praticos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios-praticos/script.js
@@ -88,16 +88,139 @@ let captureHoliday = document.querySelectorAll(".holiday");
 
 let captureButton = document.querySelector("#btn-holiday");
 
-captureButton.addEventListener("click", function() {
-    for(let index = 0; index < captureHoliday.length; index += 1) { 
-        if (captureHoliday[index].style.backgroundColor === "") {
-            captureHoliday[index].style.backgroundColor = "rgb(76,164,109)";
-        } else {
-            captureHoliday[index].style.backgroundColor = ""
-        }
-    }  
-    
-})
+captureButton.addEventListener("click", function () {
+  for (let index = 0; index < captureHoliday.length; index += 1) {
+    if (captureHoliday[index].style.backgroundColor === "") {
+      captureHoliday[index].style.backgroundColor = "rgb(76,164,109)";
+    } else {
+      captureHoliday[index].style.backgroundColor = "";
+    }
+  }
+});
 
-console.log(captureHoliday);
+/* Exercício 4:
+Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+Adicione a este botão o ID "btn-friday" .
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container" . */
 
+function createButton2(nameButton2) {
+  let newBtton = document.createElement("button");
+  captureDivButton.appendChild(newBtton);
+  newBtton.id = "btn-friday";
+  newBtton.innerHTML = nameButton2;
+}
+
+createButton2("Sexta-Feira");
+
+/* Exercício 5:
+Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias. */
+
+function eventProcess(arrayFridays) {
+  let captureButton2 = document.querySelector("#btn-friday");
+  let captureFriday = document.querySelectorAll(".friday");
+
+  captureButton2.addEventListener("click", function () {
+    for (let index = 0; index < captureFriday.length; index += 1) {
+      if (captureFriday[index].innerHTML !== "Sextou") {
+        captureFriday[index].innerHTML = "Sextou";
+      } else {
+        captureFriday[index].innerHTML = arrayFridays[index];
+      }
+    }
+  });
+}
+
+let fridays = [4, 11, 18, 25];
+
+eventProcess(fridays);
+
+/* Exercício 6:
+Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
+
+Dica - Propriedade: event.target . */
+
+let captureDezembro = document.querySelector("#month");
+
+captureDezembro.addEventListener("mouseenter", zoom1);
+
+function zoom1(event) {
+  event.target.style.fontSize = "40px";
+}
+
+captureDezembro.addEventListener("mouseleave", zoom2);
+
+function zoom2(event) {
+  event.target.style.fontSize = "20px";
+}
+
+/* Exercício 7:
+Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" . */
+
+let captureMyTasks = document.querySelector(".my-tasks");
+
+function addTasks(nameTasks) {
+  let newSpan = document.createElement("span");
+  captureMyTasks.appendChild(newSpan);
+  newSpan.innerHTML = nameTasks;
+}
+
+addTasks("Estudar");
+
+/* Exercício 8:
+Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" . */
+
+function createNewDiv(nameColor) {
+  let newDiv = document.createElement("div");
+  captureMyTasks.appendChild(newDiv);
+  newDiv.classList.add("task");
+  newDiv.style.backgroundColor = nameColor;
+}
+
+createNewDiv("rgb(76,164,109)");
+
+/* Exercício 9:
+Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
+Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+ */
+
+let captureNewDiv = captureMyTasks.lastElementChild;
+
+captureNewDiv.addEventListener("click", selectedTaskColor);
+
+function selectedTaskColor(event) {
+  if (event.target.className === "task") {
+    event.target.className = "task-selected";
+  } else {
+    event.target.className = "task";
+  }
+}
+
+/* Exercício 10:
+Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+ */
+
+function clickDay() {
+  let captureAllDays = document.querySelectorAll(".day");
+  let captureTaskSelected = document.getElementsByClassName("task-selected");
+  let captureColor = captureNewDiv.style.backgroundColor;
+
+  for (let index = 0; index < captureAllDays.length; index += 1) {
+    captureAllDays[index].addEventListener("click", function (event) {
+      let daysColors = event.target.style.backgroundColor;
+      if (daysColors !== captureColor && captureTaskSelected.length > 0) {
+        event.target.style.backgroundColor = captureColor;
+      } else if (daysColors === captureColor) {
+        event.target.style.backgroundColor = "rgb(239,239,239)";
+      }
+    });
+  }
+}
+
+clickDay();
+
+console.log();
