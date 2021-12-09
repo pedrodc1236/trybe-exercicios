bd0f9c7c04de131c934c612e01caefdda87bd943
Pedro Tavares <pedrodc1236@gmail.com>
2021-12-09 03:08:08 -0300

    exercicios praticos concluidos, porém falta o localstarage

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/exercicios-praticos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/exercicios-praticos/script.js
@@ -1,35 +1,29 @@
-/* let captureBody = document.getElementsByTagName("body")[0];
+let captureBody = document.querySelector("body");
 
-function backgroundColorScreen(nameBackgroundColor) {
-  let captureFirstButton = document.getElementById("backgroundColorScreen");
-
-  captureFirstButton.addEventListener("click", function () {
-    captureBody.style.backgroundColor = nameBackgroundColor;
-  });
-}
-
-backgroundColorScreen("green");
-console.log(captureFirstButton) */
+window.onload = function () {
+  let colorBody = localStorage.getItem("backgroundScreen");
+  captureBody.style.backgroundColor = colorBody;
+};
 
 // Botoes de fundo de tela
 
 //Cor de fundo da tela
 
-let captureBody = document.querySelector("body");
-
 const captureButtons = document.querySelectorAll(".backgroundScreen");
 
-const arrayBackgroundColors = ["green", "white", "purple"];
+const arrayBackgroundColors = ["#7C9772", "#C7D1CB", "#F3F9D2"];
 
 for (let index = 0; index < captureButtons.length; index += 1) {
   captureButtons[index].addEventListener("click", function () {
     captureBody.style.backgroundColor = arrayBackgroundColors[index];
+
+    localStorage.setItem("backgroundScreen", arrayBackgroundColors[index]);
   });
 }
 
 //Cor do texto
 
-const arrayColorText = ["purple", "red", "brown"];
+const arrayColorText = ["#173753", "#B8B8F3", "#EBEBEB"];
 
 const captureButtons2 = document.querySelectorAll(".colorText");
 
@@ -38,5 +32,44 @@ for (let index = 0; index < captureButtons2.length; index += 1) {
     captureBody.style.color = arrayColorText[index];
   });
 }
+
+//Tamanho da fonte
+
+const arrayFontSize = ["12px", "14px", "16px"];
+
+const captureButtons3 = document.querySelectorAll(".fontSize");
+
+for (let index = 0; index < captureButtons3.length; index += 1) {
+  captureButtons3[index].addEventListener("click", function () {
+    captureBody.style.fontSize = arrayFontSize[index];  
+  });
+}
+
+//Espaçamento entre as linhas
+
+const arrayLineHeight = ["25px", "30px", "35px"];
+
+const captureButtons4 = document.querySelectorAll(".lineHeight");
+
+for(let index = 0; index < captureButtons4.length; index += 1) {
+    captureButtons4[index].addEventListener("click", function () {
+        captureBody.style.lineHeight = arrayLineHeight[index];        
+    })
+}
+
+//Forte do texto
+
+const arrayFontFamily = ["sans-serif", "monospace","inherit"];
+
+const captureButtons5 = document.querySelectorAll(".fontFamily");
+
+for(let index = 0; index < captureButtons5.length; index += 1) {
+    captureButtons5[index].addEventListener("click", function(){
+        captureBody.style.fontFamily = arrayFontFamily[index];
+    })
+}
+
+// falta fazer o localstarage em 4 arquivos
+
 
 
