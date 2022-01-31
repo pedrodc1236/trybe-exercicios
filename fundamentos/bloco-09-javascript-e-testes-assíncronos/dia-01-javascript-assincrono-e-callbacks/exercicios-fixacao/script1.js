aaf006aa538b5cf503c35c04e6c9ce5751003f3a
Pedro Tavares <pedrodc1236@gmail.com>
2022-01-31 14:36:36 -0300

    9.1 falta as atividades

---
+++ fundamentos/bloco-09-javascript-e-testes-assíncronos/dia-01-javascript-assincrono-e-callbacks/exercicios-fixacao/script1.js
@@ -0,0 +1,20 @@
+// 1 - Adicione uma callback como parâmetro da funcão getUser .
+// No código abaixo você tem a função getUser , que define um objeto com os dados de uma pessoa. Complete a função getUser de forma que ela receba uma função callback como parâmetro para realizar as operações abaixo:
+// Insira o retorno da função getUser ;
+// Complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich";
+// Complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian".
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return param(userToReturn);
+};
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
